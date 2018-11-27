85153f1e0ae65c0f241c9cdbef348b3112ddcfb2
Benjamin Hill <benhill@google.com>
2018-11-27 09:47:23 -0800

    for https://github.com/tzolov/mtcnn-java/issues/5

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 
 
-For example files and usage:
-https://github.com/mmilovec/facedetectionOpenCV/blob/master/face_detector_image.py
-(but what if we don't want to scale to 300x300?)
-
-
-https://github.com/opencv/opencv/blob/master/samples/android/mobilenet-objdetect/src/org/opencv/samples/opencv_mobilenet/MainActivity.java
-
-https://www.pyimagesearch.com/2017/11/06/deep-learning-opencvs-blobfromimage-works/
-
-
-https://github.com/yeephycho/tensorflow-face-detection
+Based on library and sample in https://github.com/tzolov/mtcnn-java/blob/master/src/test/java/net/tzolov/cv/mtcnn/sample/FaceDetectionSample1.java
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,67 @@
 import net.tzolov.cv.mtcnn.MtcnnService
-import net.tzolov.cv.mtcnn.MtcnnUtil
+import java.awt.BasicStroke
+import java.awt.Color
+import java.awt.Point
+import java.awt.RenderingHints
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
 fun main() {
+    println(System.getProperty("java.version"))
+    println(System.getProperty("os.name"))
+    println(System.getProperty("os.version"))
+    println(System.getProperty("os.arch"))
+
     val mtcnnService = MtcnnService()
-    val inputImage = MtcnnUtil.to3ByteBGR(ImageIO.read(File("pivotal-ipo-nyse.jpg")))!!
+    val inputImage = ImageIO.read(File("DSC02953_cropped.jpg"))
+            .to3ByteBGR()
+
     println("Loaded image as BGR: ${inputImage.width}x${inputImage.height} ${inputImage.type}")
-    val faceAnnotations = mtcnnService.faceDetection(inputImage)
-    val annotatedImage = MtcnnUtil.drawFaceAnnotations(inputImage, faceAnnotations)
-    ImageIO.write(annotatedImage, "png", File("./AnnotatedImage.png"))
+    val tileMap = inputImage.toOverlappingTiles()
+    println("Working with ${tileMap.size} tiles")
+
+    val faces = tileMap.map { (location, image) ->
+
+        val faceAnnotations = mtcnnService.faceDetection(image)!!
+        println("${location.x}x${location.y} = ${faceAnnotations.size}")
+        location to faceAnnotations.filterNotNull()
+    }
+
+    println("Found all faces with overlap: Tiles: ${faces.size}, Faces: ${faces.sumBy { it.second.size }}")
+    inputImage.createGraphics().let { g2d ->
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        g2d.stroke = BasicStroke(3.0f)
+        g2d.color = Color.YELLOW
+
+        faces.forEach { (offset, fas) ->
+            fas.forEach { fa ->
+                g2d.drawRect(offset.x + fa.boundingBox.x, offset.y + fa.boundingBox.y, fa.boundingBox.w, fa.boundingBox.h)
+            }
+        }
+        g2d.dispose()
+    }
+    ImageIO.write(inputImage, "png", File("allFaces.png"))
+}
+
+
+// Use MtcnnUtil in next version
+fun BufferedImage.to3ByteBGR(): BufferedImage {
+    if (type == BufferedImage.TYPE_3BYTE_BGR) {
+        return this
+    }
+    val outputImage = BufferedImage(width, this.height, BufferedImage.TYPE_3BYTE_BGR)
+    outputImage.graphics.drawImage(this, 0, 0, null)
+    return outputImage
+}
+
+fun BufferedImage.toOverlappingTiles(res: Int = 400): Map<Point, BufferedImage> {
+    return (0..(height - res) step res / 2).flatMap { y ->
+        (0..(width - res) step res / 2).map { x ->
+            Point(x, y)
+        }
+    }.map { point ->
+        point to getSubimage(point.x, point.y, res, res)
+    }.toMap()
 }
