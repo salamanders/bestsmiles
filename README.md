8603288aa9d5724eb6d1441530995cb5ddefeb13
Benjamin Hill <benhill@google.com>
2018-11-28 09:54:02 -0800

    simplified

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 
 
 Based on library and sample in https://github.com/tzolov/mtcnn-java/blob/master/src/test/java/net/tzolov/cv/mtcnn/sample/FaceDetectionSample1.java
+
+400px squares = 
+308 faces
+3.7 minutes
+
+
+200px squares =
+314 faces
+1.5 minutes
+
+
+100px sq =
+302 faces
+0.7 minutes
+ 
+    inputImage.createGraphics()!!.let { g2d ->
+    g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    g2d.stroke = BasicStroke(3.0f)
+    g2d.color = Color.YELLOW
+    
+    faces.forEach {
+        g2d.drawRect(it.boundingBox.x, it.boundingBox.y, it.boundingBox.w, it.boundingBox.h)
+    }
+    g2d.dispose()
+    }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,62 +1,61 @@
+package info.benjaminhill.bestsmiles
+
+import mu.KotlinLogging
+import net.tzolov.cv.mtcnn.FaceAnnotation
 import net.tzolov.cv.mtcnn.MtcnnService
-import java.awt.BasicStroke
-import java.awt.Color
 import java.awt.Point
-import java.awt.RenderingHints
+import java.awt.Rectangle
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-fun main() {
-    println(System.getProperty("java.version"))
-    println(System.getProperty("os.name"))
-    println(System.getProperty("os.version"))
-    println(System.getProperty("os.arch"))
+private val logger = KotlinLogging.logger {}
 
-    val mtcnnService = MtcnnService()
-    val inputImage = ImageIO.read(File("DSC02953_cropped.jpg"))
-            .to3ByteBGR()
+fun FaceAnnotation.offset(loc: Point): FaceAnnotation {
+    val newFa = FaceAnnotation()
+    newFa.boundingBox = FaceAnnotation.BoundingBox.of(boundingBox.x + loc.x, boundingBox.y + loc.y, boundingBox.w, boundingBox.h)
+    newFa.confidence = confidence
+    newFa.landmarks = landmarks.map { FaceAnnotation.Landmark.of(it.type, FaceAnnotation.Landmark.Position.of(it.position.x + loc.x, it.position.y + loc.y)) }.toTypedArray()
+    return newFa
+}
 
-    println("Loaded image as BGR: ${inputImage.width}x${inputImage.height} ${inputImage.type}")
-    val tileMap = inputImage.toOverlappingTiles()
-    println("Working with ${tileMap.size} tiles")
+fun FaceAnnotation.BoundingBox.toRectangle() = Rectangle(x, y, w, h)
 
-    val faces = tileMap.map { (location, image) ->
+fun FaceAnnotation.BoundingBox.intersects(other: FaceAnnotation.BoundingBox): Boolean = this.toRectangle().intersects(other.toRectangle())
 
-        val faceAnnotations = mtcnnService.faceDetection(image)!!
-        println("${location.x}x${location.y} = ${faceAnnotations.size}")
-        location to faceAnnotations.filterNotNull()
+fun getFaces(image: BufferedImage, mtcnnService: MtcnnService = MtcnnService()): List<FaceAnnotation> {
+    val bgr = image.to3ByteBGR()
+    val tileMap = bgr.toOverlappingTiles()
+    logger.debug { "Created ${tileMap.size} overlapping tiles." }
+    val overlappingFaces = tileMap.flatMap { (location, image) ->
+        mtcnnService.faceDetection(image)!!.filterNotNull().also {
+            logger.debug { "${location.x}x${location.y} = ${it.size}" }
+        }.map {
+            it.offset(location)
+        }
     }
 
-    println("Found all faces with overlap: Tiles: ${faces.size}, Faces: ${faces.sumBy { it.second.size }}")
-    inputImage.createGraphics().let { g2d ->
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g2d.stroke = BasicStroke(3.0f)
-        g2d.color = Color.YELLOW
-
-        faces.forEach { (offset, fas) ->
-            fas.forEach { fa ->
-                g2d.drawRect(offset.x + fa.boundingBox.x, offset.y + fa.boundingBox.y, fa.boundingBox.w, fa.boundingBox.h)
+    val nonOverlappingFaces = mutableListOf<FaceAnnotation>()
+    // Filter overlaps
+    overlappingFaces
+            .sortedByDescending { it.confidence }
+            .filterTo(nonOverlappingFaces) { nextFa ->
+                nonOverlappingFaces.firstOrNull { existingFa ->
+                    existingFa.boundingBox.intersects(nextFa.boundingBox)
+                } == null
             }
-        }
+    return nonOverlappingFaces
+}
+
+fun BufferedImage.copyImage(): BufferedImage = BufferedImage(width, height, type).apply {
+    createGraphics()!!.let { g2d ->
+        g2d.drawImage(this, 0, 0, null)
         g2d.dispose()
     }
-    ImageIO.write(inputImage, "png", File("allFaces.png"))
 }
 
-
-// Use MtcnnUtil in next version
-fun BufferedImage.to3ByteBGR(): BufferedImage {
-    if (type == BufferedImage.TYPE_3BYTE_BGR) {
-        return this
-    }
-    val outputImage = BufferedImage(width, this.height, BufferedImage.TYPE_3BYTE_BGR)
-    outputImage.graphics.drawImage(this, 0, 0, null)
-    return outputImage
-}
-
-fun BufferedImage.toOverlappingTiles(res: Int = 400): Map<Point, BufferedImage> {
+/** Not a huge memory drain because subimage uses same backing data */
+fun BufferedImage.toOverlappingTiles(res: Int = 200): Map<Point, BufferedImage> {
     return (0..(height - res) step res / 2).flatMap { y ->
         (0..(width - res) step res / 2).map { x ->
             Point(x, y)
@@ -65,3 +64,30 @@ fun BufferedImage.toOverlappingTiles(res: Int = 400): Map<Point, BufferedImage> 
         point to getSubimage(point.x, point.y, res, res)
     }.toMap()
 }
+
+// Use MtcnnUtil in next version
+fun BufferedImage.to3ByteBGR(): BufferedImage = if (type == BufferedImage.TYPE_3BYTE_BGR) {
+    this
+} else {
+    BufferedImage(width, this.height, BufferedImage.TYPE_3BYTE_BGR).apply {
+        createGraphics()!!.let { g2d ->
+            g2d.drawImage(this, 0, 0, null)
+            g2d.dispose()
+        }
+    }
+}
+
+fun main() {
+    logger.debug { "Java: ${System.getProperty("java.version")}" }
+    logger.debug { "OS: ${System.getProperty("os.name")} ${System.getProperty("os.version")} ${System.getProperty("os.arch")}" }
+    logger.debug { "Hardware: Cores:${Runtime.getRuntime().availableProcessors()}, RAM(gb):${Runtime.getRuntime().freeMemory() / 1_073_741_824.0}" }
+
+    val mtcnnService = MtcnnService()
+    val imageFiles = File(System.getProperty("user.home"), "/Desktop/bestsmiles_in").walk().filter { it.extension.toLowerCase() == "jpg" }.toList().sorted()
+    println("Total files: ${imageFiles.size}")
+
+    imageFiles.forEach { file ->
+        println("${file.name}\t${getFaces(ImageIO.read(file).to3ByteBGR(), mtcnnService).size}")
+    }
+}
+
